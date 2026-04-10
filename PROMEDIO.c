b921b07417f0f1318eae8ed8c87853bfc631169f
darioalexis777-cmd <darioalexis777@gmail.com>
2026-04-10 10:50:28 -0500

    Cambio1

--- PROMEDIO.c
+++ PROMEDIO.c
@@ -3,11 +3,7 @@
 float promedio(float n1, float n2, float n3);
 int main (){
 
-
     float p1,p2,p3;
-
-
-
     float pf;
     printf("ingrese Parcial 1: ");
     scanf("%f",&p1);
@@ -19,9 +15,6 @@ int main (){
 
       printf("ingrese Parcial 3: ");
     scanf("%f",&p3);
-
-
-
 
     pf = promedio(p1,p2,p3);
     printf("promedio total =%2f\n",pf);
@@ -35,13 +28,10 @@ if (pf > 13.99) {
     }
 
         return 0;
-
-
 }
 
 float promedio (float n1, float n2, float n3){
      float resultado;
-
 
     resultado = (n1+n2+n3) /3 ;
     return resultado;
